66b879f2aacd4f82b943df3aa2fb5acb01118a22
e <xjshuanglong@126.com>
2017-01-04 23:33:27 +0800

    directory test

--- GameAndroidControlPCCoding/GenericUtils/GenericUtilsTest.cpp
+++ GameAndroidControlPCCoding/GenericUtils/GenericUtilsTest.cpp
@@ -14,4 +14,19 @@ void GenericUtilsTestEntry(ShuangLong::ILog *pLog)
 
 	g_pLog->Console("Log Testing");
 	g_pLog->Console(SL_CODELOCATION, "%d --> %s", 110, "Location Testing");
+
+	char curDirBuffer[512];
+	GetCurrentDirectoryA(512, curDirBuffer);
+	g_pLog->Console("Current Directory: %s", curDirBuffer);
+
+	char fullPathBuffer[512];
+	char *pFilePart = nullptr;
+	GetFullPathNameA("Log.h", 512, fullPathBuffer, &pFilePart);
+	g_pLog->Console("Full Path: %s", fullPathBuffer);
+	g_pLog->Console("File Name : %s", pFilePart);
+
+	DWORD fileAttributes = GetFileAttributesA("Log.h");
+	g_pLog->Console("File Attribut : %d", fileAttributes);
+
+	//GetFileSize()
 }
