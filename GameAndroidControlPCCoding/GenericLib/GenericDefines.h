501d6cea1b599f925da29b8848bf7563dbafced1
xautshuanglong <xjshuanglong@126.com>
2017-10-09 10:51:09 +0800

    macro define check platform

--- GameAndroidControlPCCoding/GenericLib/GenericDefines.h
+++ GameAndroidControlPCCoding/GenericLib/GenericDefines.h
@@ -12,10 +12,34 @@
 #else
 #endif
 
-#if GENERICUTILS_EXPORTS
-#define GENERIC_EXPORT __declspec(dllexport)
+#ifdef _USRDLL
+#  ifdef GENERICUTILS_EXPORTS
+#      define GENERIC_EXPORT __declspec(dllexport)
+#  else
+#      define GENERIC_EXPORT __declspec(dllimport)
+#  endif
 #else
-#define GENERIC_EXPORT __declspec(dllimport)
+#  define GENERIC_EXPORT
+#endif // _USRDLL
+
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
+#  define OS_WINDOWS
+#elif defined(__CYGWIN__) || defined(__CYGWIN32__)
+#  define OS_CYGWIN
+#elif defined(linux) || defined(__linux) || defined(__linux__)
+#  ifndef OS_LINUX
+#  define OS_LINUX
+#  endif
+#elif defined(macintosh) || defined(__APPLE__) || defined(__APPLE_CC__)
+#  define OS_MACOSX
+#elif defined(__FreeBSD__)
+#  define OS_FREEBSD
+#elif defined(__NetBSD__)
+#  define OS_NETBSD
+#elif defined(__OpenBSD__)
+#  define OS_OPENBSD
+#else
+// TODO: Add other platforms.
 #endif
 
 #define DECLARE_SHARED_POINTER(clsName) typedef std::shared_ptr<clsName> Ptr##clsName;
