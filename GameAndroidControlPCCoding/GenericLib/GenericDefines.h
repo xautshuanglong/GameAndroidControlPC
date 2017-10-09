d6acdc8684b4e45c2eeb510972a9100ce0256844
xautshuanglong <xjshuanglong@126.com>
2017-10-09 10:55:13 +0800

    whether have defined

--- GameAndroidControlPCCoding/GenericLib/GenericDefines.h
+++ GameAndroidControlPCCoding/GenericLib/GenericDefines.h
@@ -23,21 +23,33 @@
 #endif // _USRDLL
 
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
+#  ifndef OS_WINDOWS
 #  define OS_WINDOWS
+#  endif
 #elif defined(__CYGWIN__) || defined(__CYGWIN32__)
+#  ifndef OS_CYGWIN
 #  define OS_CYGWIN
+#  endif
 #elif defined(linux) || defined(__linux) || defined(__linux__)
 #  ifndef OS_LINUX
 #  define OS_LINUX
 #  endif
 #elif defined(macintosh) || defined(__APPLE__) || defined(__APPLE_CC__)
+#  ifndef OS_MACOSX
 #  define OS_MACOSX
+#  endif
 #elif defined(__FreeBSD__)
+#  ifndef OS_FREEBSD
 #  define OS_FREEBSD
+#  endif
 #elif defined(__NetBSD__)
+#  ifndef OS_NETBSD
 #  define OS_NETBSD
+#  endif
 #elif defined(__OpenBSD__)
+#  ifndef OS_OPENBSD
 #  define OS_OPENBSD
+#  endif
 #else
 // TODO: Add other platforms.
 #endif
