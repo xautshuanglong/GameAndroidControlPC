25e2bbfd0c3ca1497496f0769f0877e5b445ec32
xautshuanglong <xjshuanglong@126.com>
2017-10-13 19:42:21 +0800

    add MainWindow

---
+++ GameAndroidControlPCCoding/ShuanglongTools/MainWindow.h
@@ -0,0 +1,34 @@
+#pragma once
+
+namespace Shuanglong
+{
+    namespace UI
+    {
+        using namespace DuiLib;
+        class MainWindow : public DuiLib::WindowImplBase
+        {
+        private:
+            DUI_DECLARE_MESSAGE_MAP()
+
+        public:
+            MainWindow();
+            ~MainWindow();
+
+            LPCTSTR GetWindowClassName() const;
+            virtual DuiLib::CDuiString GetSkinFolder();
+            virtual DuiLib::CDuiString GetSkinFile();
+            virtual void OnFinalMessage(HWND hWnd);
+            virtual void InitWindow();
+            //virtual LRESULT ResponseDefaultKeyEvent(WPARAM wParam);
+            virtual LRESULT OnClose(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled);
+            virtual LRESULT OnDestroy(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled);
+
+        protected:
+            //void Notify(DuiLib::TNotifyUI& msg);
+            void OnPrepare(DuiLib::TNotifyUI& msg);
+            void OnClick(DuiLib::TNotifyUI& msg);
+            void OnTimer(DuiLib::TNotifyUI& msg);
+            void OnExit(DuiLib::TNotifyUI& mag);
+        };
+    }
+}
--- GameAndroidControlPCCoding/ShuanglongTools/stdafx.h
+++ GameAndroidControlPCCoding/ShuanglongTools/stdafx.h
@@ -17,5 +17,5 @@
 #include <memory.h>
 #include <tchar.h>
 
-
 // TODO: reference additional headers your program requires here
+#include <UIlib.h>
