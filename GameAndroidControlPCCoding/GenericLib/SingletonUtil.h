e053d8dfdcfa4fa8c8e93398a2edc7440e5319d4
xjshuanglong <xjshuanglong@126.com>
2018-12-14 18:10:02 +0800

    SingletonUtil Test

---
+++ GameAndroidControlPCCoding/GenericLib/SingletonUtil.h
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace Shuanglong
+{
+    namespace Utils
+    {
+
+        /************************************************************************\
+         *    模板单例
+         *    智能单例模式
+        \************************************************************************/
+        template <typename T>
+        class SingletonUtil
+        {
+        public:
+            SingletonUtil();
+            ~SingletonUtil();
+        };
+    }
+}
